d28667639d5ec70f858f8392c8690dff235ce976
angelobrando <killclad@gmail.com>
2019-12-10 10:07:17 +0100

    Create index.php
    opdracht2

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Boodschappen</title>
+</head>
+<body>
+<?php
+$boodschap = "aardappel";
+?> 
+<p>Ik moet vandaag <?php echo $boodschap . "en" ?>  kopen.</p>
+</body>
+</html> 
