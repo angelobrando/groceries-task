89c853a6bcb02b666ec5dd2a9e4aa1d3ccb4f021
angelobrando <killclad@gmail.com>
2019-12-10 10:34:31 +0100

    opdracht3.5

--- opdracht3.php
+++ opdracht3.php
@@ -7,10 +7,10 @@
 $boodschappen = ["aardappelen", "aardbeien", "3 pakken melk", "yoghurt"];
 ?> 
 <ol style="list-style-type:disc;">
-    <li><?php echo $boodschappen[0] ?></li>
-    <li><?php echo $boodschappen[1] ?></li>
-    <li><?php echo $boodschappen[2] ?></li>
-    <li><?php echo $boodschappen[3] ?></li>
+    <li><?php echo $boodschappen[0] ?><img src="potato.png"></li>
+    <li><?php echo $boodschappen[1] ?><img src="strawberry.png"></li>
+    <li><?php echo $boodschappen[2] ?><img src="milk.png"></li>
+    <li><?php echo $boodschappen[3] ?><img src="yoghurt.png"></li>
 </ol>
 </body>
 </html> 
