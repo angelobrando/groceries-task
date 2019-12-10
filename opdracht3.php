5ead1664b08916ec4c7a427f2c84676918286d7c
angelobrando <killclad@gmail.com>
2019-12-10 10:22:00 +0100

    opdracht3
    opdracht3

---
+++ opdracht3.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Boodschappen</title>
+</head>
+<body>
+<?php
+$boodschappen = ["aardappelen", "aardbeien", "3 pakken melk", "yoghurt"];
+?> 
+<ol style="list-style-type:disc;">
+    <li><?php echo $boodschappen[0] ?></li>
+    <li><?php echo $boodschappen[1] ?></li>
+    <li><?php echo $boodschappen[2] ?></li>
+    <li><?php echo $boodschappen[3] ?></li>
+</ol>
+</body>
+</html> 
